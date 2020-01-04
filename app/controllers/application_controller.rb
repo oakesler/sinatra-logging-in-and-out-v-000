55f4b405b415ec669408e1331a8641a55dc23ad9
Olivia Kesler <oakesler@protonmail.ch>
2020-01-04 08:26:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,6 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     if Helpers.is_logged_in?(session)
-      @user = Helpers.current_user(session)
       erb :account
     else
       #redirect to("/error")
